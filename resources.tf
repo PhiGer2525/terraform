34c0d8d35dc0f77d4ab64b431c44f670fdb696be
Gerstenberger <ger0001p@c701380.belgium.fhm.de>
2019-03-26 11:37:11 +0100

    PG

--- resources.tf
+++ resources.tf
@@ -1,16 +1,17 @@
 #VPC anlegen
 resource "aws_vpc" "environment-example-two" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
-  enable_dns_support = true
+  enable_dns_support   = true
+
   tags {
-    Name = "terraform-aws-vpc-example-two"
+    Name = "terraform-aws-vpc-example"
   }
 }
 
 #Subnet anlegen
 resource "aws_subnet" "subnet_1" {
-  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
-  vpc_id = "${aws_vpc.environment-example-two.id}"
+  cidr_block        = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block, 3, 1)}"
+  vpc_id            = "${aws_vpc.environment-example-two.id}"
   availability_zone = "eu-central-1a"
 }
